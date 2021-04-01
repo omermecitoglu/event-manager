d2d56e32b3a2cc9a140907bcf1824379cced111e
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2021-04-01 04:46:56 +0300

    added index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+class EventManager {
+
+	constructor() {
+		this.list = {};
+	}
+
+	adjust(event) {
+		if(!this.list[event]) {
+			this.list[event] = [];
+		}
+	}
+
+	on(event, fn) {
+		this.adjust(event);
+		const idx = this.list[event].indexOf(fn);
+		if(idx < 0) this.list[event].push(fn);
+	}
+
+	off(event, fn) {
+		this.adjust(event);
+		const idx = this.list[event].indexOf(fn);
+		if(idx > -1) this.list[event].splice(idx, 1);
+	}
+
+	trigger(event, ...args) {
+		this.adjust(event);
+		for(let fn of this.list[event]) {
+			fn.apply(null, args);
+		}
+	}
+}
+
+module.exports = EventManager;
