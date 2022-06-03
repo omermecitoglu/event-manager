c7cd7c241dfcc8056f9adf4a86aa39ca020005b0
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2022-06-03 18:16:00 +0300

    added eslint

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 export default class EventManager {
   private list: Record<string, Array<(...args: unknown[]) => void>>;
 
-	constructor() {
-		this.list = {};
-	}
+  constructor() {
+    this.list = {};
+  }
 
-	private adjust(event: string) {
-		if(!this.list[event]) {
-			this.list[event] = [];
-		}
-	}
+  private adjust(event: string) {
+    if (!this.list[event]) {
+      this.list[event] = [];
+    }
+  }
 
   private cleanUp(event: string) {
     if (!this.list[event].length) {
@@ -17,28 +17,28 @@ export default class EventManager {
     }
   }
 
-	on(event: string, fn: () => void) {
-		this.adjust(event);
-		const idx = this.list[event].indexOf(fn);
-		if(idx < 0) this.list[event].push(fn);
-	}
+  on(event: string, fn: () => void) {
+    this.adjust(event);
+    const idx = this.list[event].indexOf(fn);
+    if (idx < 0) this.list[event].push(fn);
+  }
 
-	off(event: string, fn: () => void) {
-		this.adjust(event);
-		const idx = this.list[event].indexOf(fn);
-		if(idx > -1) this.list[event].splice(idx, 1);
+  off(event: string, fn: () => void) {
+    this.adjust(event);
+    const idx = this.list[event].indexOf(fn);
+    if (idx > -1) this.list[event].splice(idx, 1);
     this.cleanUp(event);
-	}
+  }
 
-	trigger(event: string, ...args: unknown[]) {
-		this.adjust(event);
-		for(let fn of this.list[event]) {
-			try {
-				fn.apply(null, args);
-			} catch(error) {
-				console.log("event error: " + event + " <----------");
-				console.error(error);
-			}
-		}
-	}
+  trigger(event: string, ...args: unknown[]) {
+    this.adjust(event);
+    for (const fn of this.list[event]) {
+      try {
+        fn(...args);
+      } catch (error) {
+        console.log("event error: " + event + " <----------");
+        console.error(error);
+      }
+    }
+  }
 }
